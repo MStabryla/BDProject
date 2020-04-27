6f53cd18d2550fd20ace0507a465f17616467cf3
Mateusz Stabryła <maattizdabrowy@op.pl>
2020-04-27 16:12:13 +0200

    Stworzenie Dwóch projektów w .NET Core

---
+++ project/src/BDProject.Core/Connection.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BDProject.Core
+{
+    public class Connection {
+        private const string connectionString = "Server=tcp:{your_server_address},1433;Initial Catalog=stabryla;Persist Security Info=False;User ID={your_login};Password={your_password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        private SqlConnection databaseConnection;
+
+        public Connection(string serverAddress, string login,string password){
+            Connect(serverAddress,login,password);
+        }
+        public Connection(){
+        }
+        public string DatabaseAddress {
+            get => databaseConnection != null ? databaseConnection.DataSource : "";
+        }
+        public ConnectionState ConnectionStatus {
+            get => databaseConnection.State;
+        }
+        public void Connect(string serverAddress, string login,string password){
+            if(databaseConnection != null)
+                return;
+            databaseConnection = new SqlConnection(connectionString.Replace("{your_server_address}",serverAddress).Replace("{your_login}",login).Replace("{your_password}",password));
+            databaseConnection.Open();
+            
+        }
+        public string Query(string sqlquery){
+            string result = "";
+            SqlCommand command = new SqlCommand(sqlquery,databaseConnection);
+            using(SqlDataReader reader = command.ExecuteReader())
+            {
+                for(int i=0;i<reader.VisibleFieldCount;i++)
+                {
+                    result += reader.GetName(i) + "\t";
+                }
+                result += "\n";
+                while (reader.Read())
+                {
+                    for(int i=0;i<reader.VisibleFieldCount;i++)
+                        result += reader[i].ToString() + ( i<reader.VisibleFieldCount-1 ? "\t" : "");
+                    result += "\n";
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ project/src/BDProject.Terminal/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using BDProject.Core;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+
+namespace BDProject.Terminal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool connectionEstablished = false;
+            bool useConfig = args.Length > 0;
+            Connection con = new Connection();
+            while(!connectionEstablished){
+                if(useConfig){
+                    string[] configFileLines = System.IO.File.ReadAllLines(args[0]);
+                    try{
+                        con.Connect(
+                            configFileLines[1],
+                            configFileLines[3],
+                            configFileLines[5]);
+                        if(con.ConnectionStatus == ConnectionState.Open){
+                            Console.WriteLine("Connected to " + con.DatabaseAddress);
+                            connectionEstablished = true;
+                            break;
+                        }
+                    }
+                    catch(SqlException exception){
+                        Console.WriteLine(exception.Message);
+                        useConfig = false;
+                    }
+                }
+                Console.WriteLine("Podaj parametry (adres serwera, login, hasło):");
+                try{
+                    
+                    con.Connect(
+                        Console.ReadLine(),
+                        Console.ReadLine(),
+                        ReadPassword());
+                    if(con.ConnectionStatus == ConnectionState.Open){
+                        Console.WriteLine("Connected to " + con.DatabaseAddress);
+                        connectionEstablished = true;
+                    }
+                }
+                catch(SqlException exception){
+                    Console.WriteLine(exception.Message);
+                }
+            }
+            Menu(con);
+        }
+        private static Dictionary<string,string> menuOption = new Dictionary<string, string>() {
+            {"_TABLES","SELECT * FROM INFORMATION_SCHEMA.TABLES"}
+        };
+        private static void Menu(Connection con){
+            Console.WriteLine("Specjalne operacje:");
+            foreach(KeyValuePair<string,string> elem in menuOption)
+                Console.WriteLine(elem.Key + " => '" + elem.Value +"'");
+            while(true){
+                Console.Write("> ");
+                string command = Console.ReadLine();
+                if(menuOption.ContainsKey(command)){
+                    command = menuOption[command];
+                }
+                try{
+                    Console.Write(con.Query(command));
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+        private static string ReadPassword(){
+            string pass = "";
+            do
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+                if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter)
+                {
+                    pass += key.KeyChar;
+                    Console.Write("*");
+                }
+                else
+                {
+                    if (key.Key == ConsoleKey.Backspace && pass.Length > 0)
+                    {
+                        pass = pass.Substring(0, (pass.Length - 1));
+                        Console.Write("\b \b");
+                    }
+                    else if(key.Key == ConsoleKey.Enter)
+                    {
+                        break;
+                    }
+                }
+            } while (true);
+            Console.Write("\n");
+            return pass;
+        }
+    }
+}
